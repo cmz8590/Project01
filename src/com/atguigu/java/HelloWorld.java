4c62e4336f3fe94a60b5a45941fff2ec29327c48
chenmingzhi_ext <chen.mingzhi@ustcinfo.com>
2019-07-31 20:47:03 +0800

    Initial commit

---
+++ src/com/atguigu/java/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.atguigu.java;
+
+public class HelloWorld {
+    public static final int NUM = 22;
+    /**
+     * 客户名
+     */
+    private int name;
+
+    /**
+     * id
+     */
+    private int id;
+
+
+}
